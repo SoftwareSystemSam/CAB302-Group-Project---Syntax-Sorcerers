2a54efd37f8ef8d71016420760281e6a1825f89c
Samuel Smith <georgesamsquo@hotmail.com>
2024-04-25 20:29:22 +1000

    Added comments for code so that the login and registration setup team know to add that code after user is authenticated

--- src/main/java/com/example/addressbook/SQL/UserService.java
+++ src/main/java/com/example/addressbook/SQL/UserService.java
@@ -1,5 +1,6 @@
 package com.example.addressbook.SQL;
 
+import java.sql.SQLException;
 import java.util.Objects;
 
 public class UserService {
@@ -22,9 +23,13 @@ public class UserService {
         }
     }
 
-    public User loginUser(String email, String password){
+    public User loginUser(String email, String password) throws SQLException {
         User user = userDAO.getUserByEmail(email);
         if( user!= null && Objects.equals(password, user.getPassword())){
+            // I'm leaving this here because this needs to be done after authentication has been handled and probably shouldn't be done here.
+            // Once User has been verified then you can start tracking because they should have an id in the database
+            // ActiveWindowTracker tracker = new ActiveWindowTracker(user);
+            // tracker.trackActiveWindow(); // Now you can start tracking
             // If password matches then the login is sucessful
             return user;
         }
