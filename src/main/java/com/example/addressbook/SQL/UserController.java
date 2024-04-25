949882a7718b86df7ba4925347a53accacee6b12
Samuel Smith <georgesamsquo@hotmail.com>
2024-04-25 20:43:26 +1000

    Added comment for controller file

--- src/main/java/com/example/addressbook/SQL/UserController.java
+++ src/main/java/com/example/addressbook/SQL/UserController.java
@@ -1,5 +1,6 @@
 package com.example.addressbook.SQL;
 
+// Basically a controller that lets UserService functions use the SqliteUserDAO database functions
 public class UserController {
 
     public UserController(){
