7e4a4b9c678445327bc62eb8d485a039fcdd7bab
joeyem <joeyhooper48@gmail.com>
2024-05-24 20:17:37 +1000

    Javadoc comments + code clean up to LogInController

--- src/main/java/com/example/addressbook/GUI/LogInController.java
+++ src/main/java/com/example/addressbook/GUI/LogInController.java
@@ -6,15 +6,16 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import static com.example.addressbook.HelloApplication.TITLE;
-import static java.awt.image.ImageObserver.HEIGHT;
-import static java.awt.image.ImageObserver.WIDTH;
 
+/**
+ * The LogInController class extends Application and represents the login controller of the application.
+ * It contains two buttons: "loginButton", and "signupButton".
+ * Each button is associated with an event handler that handles the button click event.
+ */
 public class LogInController extends Application {
 
     public static final String TITLE = "Screen Tracker";
@@ -25,6 +26,10 @@ public class LogInController extends Application {
     @FXML
     private Button signupButton;
 
+    /**
+     * The start method of the LogInController class.
+     * It initializes the login scene with the specified width and height, and sets the title of the stage.
+     */
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -33,7 +38,10 @@ public class LogInController extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+    /**
+     * The onLogInButtonClick method of the LogInController class.
+     * It handles the click event of the login button, and opens the LogIn scene.
+     */
     @FXML
     protected void onLogInButtonClick() throws IOException {
         try {
@@ -44,7 +52,10 @@ public class LogInController extends Application {
             e.printStackTrace();
         }
     }
-
+    /**
+     * The onSignUpButtonClick method of the LogInController class.
+     * It handles the click event of the signup button, and opens the SignUp scene.
+     */
     @FXML
     protected void onSignUpButtonClick() throws IOException {
         try {
